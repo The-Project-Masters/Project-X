9fd7394e20a7ac4f5d8911543bfaafc5887e9b27
ventsislav ivanov <venci77xxx@gmail.com>
2023-10-14 16:12:08 +0300

    fixing bug

--- template/src/common/constants.js
+++
@@ -1,9 +0,0 @@
-export const HOME = 'home';
-export const TRENDING = 'trending';
-export const FAVORITES = 'favorites';
-export const ABOUT_US = 'about_us';
-export const UPLOAD = 'upload';
-export const CONTAINER_SELECTOR = '#container';
-export const FULL_HEART = '❤';
-export const EMPTY_HEART = '♡';
-export const API_KEY = 'rQsV4ETOqd1bOXFpFD3uydMNMiU3Icu9';
--- template/src/data/giphy.js
+++
@@ -1,8 +0,0 @@
-import { API_KEY } from '../common/constants';
-
-export const getTrendingGifs = async (limit = 10) => {
-    const response = await fetch(
-        'https://api.giphy.com/v1/gifs/trending?api_key=' + API_KEY + '&limit=' + limit);
-
-    return await response.json();
-};
