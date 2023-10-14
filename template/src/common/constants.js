925716f0965d1c7dbc366276291fc75e78f16219
R@d0 <rkolev95@gmail.com>
2023-10-14 16:36:23 +0300

    Fixing

--- template/src/common/constants.js
+++ template/src/common/constants.js
@@ -1,11 +1,17 @@
 export const HOME = 'home';
-export const TRENDING = 'trending';
-export const FAVORITES = 'favorites';
-export const ABOUT_US = 'about_us';
-export const UPLOAD = 'upload';
-export const CONTAINER_SELECTOR = '#container';
-export const FULL_HEART = '❤';
-export const EMPTY_HEART = '♡';
-export const API_KEY = 'rQsV4ETOqd1bOXFpFD3uydMNMiU3Icu9';
-export const API_UPLOAD = 'https://upload.giphy.com/v1/gifs';
 
+export const CATEGORIES = 'categories';
+
+export const FAVORITES = 'favorites';
+
+export const ABOUT = 'about';
+
+export const CONTAINER_SELECTOR = '#container';
+
+export const FULL_HEART = '❤';
+
+export const EMPTY_HEART = '♡';
+
+export const TRENDING = "";
+
+export const API_KEY = "rQsV4ETOqd1bOXFpFD3uydMNMiU3Icu9";
