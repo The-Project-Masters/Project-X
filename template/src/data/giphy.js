0ed03320fc873d2ce13c3af129da455cc33d8c54
R@d0 <rkolev95@gmail.com>
2023-10-14 15:29:29 +0300

    Changes in giphy.js

--- template/src/data/giphy.js
+++ template/src/data/giphy.js
@@ -2,11 +2,7 @@ import { API_KEY } from '../common/constants';
 
 export const getTrendingGifs = async (limit = 10) => {
     const response = await fetch(
-        'https://api.giphy.com/v1/gifs/trending?api_key=' +
-            API_KEY +
-            '&limit=' +
-            limit
-    );
+        'https://api.giphy.com/v1/gifs/trending?api_key=' + API_KEY + '&limit=' + limit);
 
     return await response.json();
 };
