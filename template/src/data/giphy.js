5d4c360568c967abd2265934bcd17e27c43ee4c8
R@d0 <rkolev95@gmail.com>
2023-10-14 15:25:06 +0300

    Add giphy.js

---
+++ template/src/data/giphy.js
@@ -0,0 +1,12 @@
+import { API_KEY } from '../common/constants';
+
+export const getTrendingGifs = async (limit = 10) => {
+    const response = await fetch(
+        'https://api.giphy.com/v1/gifs/trending?api_key=' +
+            API_KEY +
+            '&limit=' +
+            limit
+    );
+
+    return await response.json();
+};
