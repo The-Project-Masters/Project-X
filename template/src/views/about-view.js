241be84849277d1c7965ae7b370ff29ac6a0fdea
ventsislav ivanov <venci77xxx@gmail.com>
2023-10-18 22:19:35 +0300

    added About info

--- template/src/views/about-view.js
+++ template/src/views/about-view.js
@@ -1,7 +1,16 @@
 export const toAboutView = () => `
 <div id="about">
   <div class="content">
-    <h1>About the app</h1>
+    <h1>About the app:</h1>
+    <p>Single Page Application (SPA) for sharing and searching funny GIFs, primarily using the Giphy API. The application allows users to perform the following essential functions:</p>
+<ul>
+    <li>Display Trending GIFs</li>
+    <li>Search GIFs</li>
+    <li>Display GIF Details</li>
+    <li>Upload GIFs</li>
+    <li>Display Uploaded GIFs</li>
+    <li>Favorite GIFs</li>
+</ul>
     <h2>Authors:  Rado & Ventsi</h2>
     <h2>Date: 2023</h2>
   </div>
