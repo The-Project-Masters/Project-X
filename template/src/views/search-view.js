faff076c5aa499104084ba7870456e9eff1e223b
R@d0 <rkolev95@gmail.com>
2023-10-18 23:08:13 +0300

    Fix Styling

--- template/src/views/search-view.js
+++ template/src/views/search-view.js
@@ -2,7 +2,7 @@ import { toSingleGifView } from './gif-view.js';
 export const toSearchView = (gifs, searchTerm, pagination) => `
 <section id="gifs" class="favoritesHome">
     <div class="container">
-      <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">GIFs found for <em>${searchTerm}</em>:</h2>
+      <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">GIFs found for "<em>${searchTerm}</em> "</h2>
       <!-- Icon Divider-->
       <div class="divider-custom">
         <div class="divider-custom-line"></div>
